f5a871e74b7792cf6ed657461889f3c22d71b9fb
unknown <Administrator@dac36.students.acts.in>
2023-01-25 15:52:23 +0530

    add number

--- Hello1.java
+++ Hello1.java
@@ -1,5 +1,11 @@
 public class Hello1{
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		
+		try(Scanner sc=new Scanner(System.in)){
+			System.out.print("A : ");
+			int a=sc.nextInt();
+			System.out.println("Hello : "+a);
+		}
 	}
 }
